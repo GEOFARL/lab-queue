2f35ccd98a57008720b8a02a55f2b4fa78e396c5
Maxim <toper.one111@gmail.com>
2023-07-27 10:18:21 +0300

    Set up routes for different pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,20 @@ import {
 } from 'react-router-dom';
 
 import Home from './pages/Home';
+import About from './pages/About';
+import Queues from './pages/Queues';
+import Polling from './pages/Polling';
+import Announcements from './pages/Announcements';
 import RootLayout from './layouts/RootLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="queues" element={<Queues />} />
+      <Route path="polling" element={<Polling />} />
+      <Route path="announcements" element={<Announcements />} />
     </Route>
   )
 );
