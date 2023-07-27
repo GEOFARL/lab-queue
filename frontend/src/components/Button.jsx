98d0379d485d2342d1dc549f78cb2d80585cd4ec
Maxim <toper.one111@gmail.com>
2023-07-27 10:21:30 +0300

    Provide interactivity to the button component

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ children, alternative }) => {
     <button
       className={`button-gradient ${
         !alternative ? 'px-6 py-1.5 md:px-8 md:py-2' : ''
-      } rounded-full`}
+      } rounded-full duration-300 transition-all hover:-translate-y-1 hover:shadow-md`}
     >
       {alternative ? (
         <div className="bg-gray px-[20px] py-1.5 md:px-[26px] md:py-2 rounded-full m-[3px]">
