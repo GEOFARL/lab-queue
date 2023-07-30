b280b3aa60056849664fa2f4105399bf2896125b
Maxim <toper.one111@gmail.com>
2023-07-30 14:54:18 +0300

    Add blurred circles

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -12,7 +12,10 @@ const RootLayout = () => {
       </div>
 
       <div className={`${styles.flexCenter} px-8 md:px-16`}>
-        <div className="w-full xl:max-w-[1100px]">
+        <div className="w-full xl:max-w-[1100px] relative">
+          {/* Background blurred circles */}
+          <div className="w-[269px] h-[269px] bg-fuchsia-600 bg-opacity-80 rounded-full blur-[200px] absolute -top-[90px] -left-[13.5rem] z-0" />
+          <div className="w-[269px] h-[269px] bg-black rounded-full blur-[518px] absolute top-[450px] -right-[75px] z-0" />
           <Outlet />
         </div>
       </div>
