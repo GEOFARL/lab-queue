4b9773192e17d7ee66450f696a67ed5189fd1466
Maxim <toper.one111@gmail.com>
2023-07-31 10:33:40 +0300

    Create error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -17,9 +18,8 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-app.get('/', (req, res, next) => {
-  res.send('hello world');
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`App is started on port ${port}`);
