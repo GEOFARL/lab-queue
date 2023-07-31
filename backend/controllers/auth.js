decdd96910e0adf4ac397753783352a4e71ac5b3
Maxim <toper.one111@gmail.com>
2023-07-31 10:28:10 +0300

    Add functionality to create users

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,18 +1,39 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
 
 // @desc    Sing up a new user
 // @route   POST /api/auth/sing-up
 // @access  Public
 const singUp = asyncHandler(async (req, res, next) => {
-  res.json('Register user');
-  // const { email, password, name, imageUri } = req.body;
-  // const user = User.find({ email });
+  const { name, email, imageUri, password, confirmPassword } = req.body;
 
-  // if (user) {
-  //   res.status(400);
-  //   throw new Error('User already exists');
-  // }
+  // Check if the user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({ name, email, imageUri, password });
+
+  if (user) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    });
+
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      maxAge: 30 * 24 * 3600 * 1000,
+      sameSite: 'strict',
+    });
+
+    res.json('Register user');
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc    Sing in user
